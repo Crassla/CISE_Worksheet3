ccfac100c6196a01b77e09d59cc8f7fd7e2a9057
Crassla <ammeyer880@gmail.com>
2023-08-02 17:24:02 +1200

    removing code from app.js for testing

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use(express.json({ extended: false }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+app.get('/', (req, res) => res.send('Hello world!'));
+
 // use Routes
 app.use('/api/books', books);
 
