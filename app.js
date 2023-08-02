6638fa5e5e397655424837edb6432b33918bf188
Crassla <ammeyer880@gmail.com>
2023-08-02 17:22:08 +1200

    removing code from app.js for testing

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 // app.js
 
 const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
 
 // routes
 const books = require('./routes/api/books');
 
 const app = express();
 
-// Connect Database
-connectDB();
-// cors
-app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: false }));
 
 // Init Middleware
 app.use(express.json({ extended: false }));
-
-app.get('/', (req, res) => res.send('Hello world!'));
 
 // use Routes
 app.use('/api/books', books);
